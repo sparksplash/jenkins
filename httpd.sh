a431a118906cc2ae5c0b375f896b94dfe132d684
sparksplash <sparksplash77@gmail.com>
2021-10-21 15:15:35 +0900

    httpd first

---
+++ httpd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo mkidr /web
+sudo yum install -y expat-devel gcc gcc-c++
+sudo wget -P /web https://downloads.apache.org/apr/apr-1.7.0.tar.gz
+sudo wget -P /web https://downloads.apache.org/apr/apr-util-1.6.1.tar.gz
+sudo wget -P /web https://downloads.apache.org/httpd/httpd-2.4.51.tar.gz
+sudo wget -P /web https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz --no-check-certificate
